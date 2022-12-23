d782fdacfe11b4d61853ed607e20e2968d14ca66
coekjan <cn_yzr@qq.com>
2022-12-24 01:27:56 +0800

    使用 `sbi_hart_start` 与 `sbi_send_ipi` 启动其他核心

--- kern/init/init.c
+++ kern/init/init.c
@@ -1,8 +1,28 @@
-#include <kern/lib/printk.h>
+#include <kern/lib/logger.h>
+#include <kern/lib/sbi.h>
+#include <layouts.h>
 #include <types.h>
 
-void kernel_init(u_int hartid) {
-  printk("[hart %d] Hello Jrinx!\n", hartid);
-  while (1) {
+void kernel_init(unsigned long hartid, unsigned long opaque) {
+  static int is_master = 1;
+  static unsigned long hart_table = 0;
+  if (is_master) {
+    printk("[ hart %ld ] Hello Jrinx, I am master hart!\n", hartid);
+    is_master = 0;
+    for (int i = 0; i < CONFIG_NR_CORES; i++) {
+      if (i != hartid) {
+        hart_table |= 1 << i;
+        sbi_hart_start(i, KERNBASE, 0);
+      }
+    }
+    sbi_send_ipi(&hart_table);
+    while (hart_table) {
+    }
+    haltk("[ hart %ld ] all cores running!", hartid);
+  } else {
+    printk("[ hart %ld ] Hello Jrinx, I am slave hart!\n", hartid);
+    hart_table &= ~(1 << hartid);
+    while (1) {
+    }
   }
 }
