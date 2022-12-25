82012875a3908d88f68b4d5b4428756dbee3e827
coekjan <cn_yzr@qq.com>
2022-12-25 14:55:13 +0800

    增加 `string.h` 头文件

---
+++ include/lib/string.h
@@ -0,0 +1,13 @@
+#ifndef _LIB_STRING_H_
+#define _LIB_STRING_H_
+
+#include <stddef.h>
+
+void *memcpy(void *restrict dst, const void *restrict src, size_t n);
+void *memset(void *restrict dst, int c, size_t n);
+size_t strlen(const char *s);
+char *strcpy(char *restrict dst, const char *restrict src);
+const char *strchr(const char *restrict s, int c);
+int strcmp(const char *restrict p, const char *restrict q);
+
+#endif
