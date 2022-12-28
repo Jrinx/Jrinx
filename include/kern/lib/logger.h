dd07ec7215ca6d06193f55251fa29e9306279371
coekjan <cn_yzr@qq.com>
2022-12-29 00:32:06 +0800

    改善 logger 的实现

--- include/kern/lib/logger.h
+++ include/kern/lib/logger.h
@@ -1,13 +1,18 @@
 #ifndef _KERN_LIB_LOGGER_H_
 #define _KERN_LIB_LOGGER_H_
 
+#include <kern/lib/hart.h>
 #include <stdarg.h>
 
 void printk(const char *restrict fmt, ...) __attribute__((format(printf, 1, 2)));
 void panick(const char *restrict fmt, ...) __attribute__((format(printf, 1, 2), noreturn));
 void haltk(const char *restrict fmt, ...) __attribute__((noreturn));
 
-#define info(msg, ...) printk("%s:%d <%s> " msg, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-#define fatal(msg, ...) panick("%s:%d <%s> " msg, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define _log_args(msg, ...)                                                                    \
+  "[ hart %ld ] %s:%d <%s> " msg, hrt_get_id(), __FILE__, __LINE__, __func__, ##__VA_ARGS__
+
+#define info(msg, ...) printk(_log_args(msg, ##__VA_ARGS__))
+#define fatal(msg, ...) panick(_log_args(msg, ##__VA_ARGS__))
+#define halt(msg, ...) haltk(_log_args(msg, ##__VA_ARGS__))
 
 #endif
--- kern/boot/init.c
+++ kern/boot/init.c
@@ -22,7 +22,7 @@ void kernel_init(unsigned long hartid, void *dtb_addr) {
     cpu_master_init = 1;
     lk_init();
 
-    info("[ hart %ld ] Hello Jrinx, I am master hart!\n", hartid);
+    info("Hello Jrinx, I am master hart!\n");
 
     struct dev_tree dt;
     panic_e(dt_load(dtb_addr, &dt));
@@ -37,12 +37,12 @@ void kernel_init(unsigned long hartid, void *dtb_addr) {
     panic_e(lk_release(&spinlock_of(init_state)));
     while (init_state != cpu_get_count()) {
     }
-    haltk("[ hart %ld ] all cores are running, halt!", hartid);
+    halt("All cores are running, halt!\n");
   } else {
     while (init_state == 0) {
     }
     vm_start();
-    info("[ hart %ld ] Hello Jrinx, I am slave hart!\n", hartid);
+    info("Hello Jrinx, I am slave hart!\n");
     panic_e(lk_acquire(&spinlock_of(init_state)));
     init_state++;
     panic_e(lk_release(&spinlock_of(init_state)));
