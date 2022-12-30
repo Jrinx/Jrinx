7745de750d9b183c39786dcd62e822ef359524d6
coekjan <cn_yzr@qq.com>
2022-12-30 14:59:11 +0800

    完善 `kern/lib/sync.h`

--- include/kern/lib/sync.h
+++ include/kern/lib/sync.h
@@ -1,8 +1,20 @@
 #ifndef _KERN_LIB_SYNC_H_
 #define _KERN_LIB_SYNC_H_
 
-#define fence(x, y) asm volatile("fence " #x ", " #y)
-#define fence_i asm volatile("fence.i")
-#define sfence_vma asm volatile("sfence.vma")
+#define fence(x, y) asm volatile("fence " #x ", " #y : : : "memory")
+#define fence_i asm volatile("fence.i" : : : "memory")
+#define sfence_vma asm volatile("sfence.vma" : : : "memory")
+
+static inline void sfence_vma_va_asid(unsigned long va, unsigned long asid) {
+  asm volatile("sfence.vma %0, %1" : : "r"(va), "r"(asid) : "memory");
+}
+
+static inline void sfence_vma_asid(unsigned long asid) {
+  asm volatile("sfence.vma x0, %0" : : "r"(asid) : "memory");
+}
+
+static inline void sfence_vma_va(unsigned long va) {
+  asm volatile("sfence.vma %0, x0" : : "r"(va) : "memory");
+}
 
 #endif
