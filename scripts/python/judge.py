b5fdd5181f925e7685fe639971b346453e36a7de
coekjan <cn_yzr@qq.com>
2023-01-18 18:25:14 +0800

    测试脚本支持描述模式串的顺序

--- scripts/python/judge.py
+++ scripts/python/judge.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+from __future__ import annotations
+from abc import abstractmethod
 import argparse
 import json
 import psutil
 import signal
 import subprocess
+from typing import Sequence
 
 from utils import *
 
@@ -32,13 +35,62 @@ class JudgeTimeout(JudgeException):
         super().__init__('Judge Timeout', message)
 
 
+class PatternTypeError(JudgeException):
+    def __init__(self, message: str) -> None:
+        super().__init__('Pattern Type Error', message)
+
+
+class Pattern:
+    def __init__(self, pattern) -> None:
+        self._pattern = pattern
+
+    @abstractmethod
+    def __call__(self, s: str) -> tuple[bool, bool, str]:  # Matched, Picked, PickedStr
+        pass
+
+    def __str__(self) -> str:
+        return str(self._pattern)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    @classmethod
+    def of(cls, pattern) -> Pattern:
+        match pattern:
+            case str():
+                return StringPattern(pattern)
+            case tuple() | list():
+                return SeqPattern(pattern)
+            case _:
+                raise PatternTypeError(f'Unsupported pattern type {type(pattern)}')
+
+
+class StringPattern(Pattern):
+    def __call__(self, s: str) -> tuple[bool, bool, str]:
+        if self._pattern in s:
+            return True, True, s
+        return False, False, None
+
+
+class SeqPattern(Pattern):
+    def __init__(self, pattern: Sequence[str]) -> None:
+        super().__init__(pattern)
+        self.__next = 0
+
+    def __call__(self, s: str) -> tuple[bool, bool, str]:
+        pattern = self._pattern[self.__next]
+        if pattern in s:
+            self.__next += 1
+            return self.__next >= len(self._pattern), True, pattern
+        return False, False, None
+
+
 class Rules:
     def __init__(self, verbose: bool, conf) -> None:
         self.__verbose = verbose
-        self.__expected = frozenset(conf['expected'])
         self.__unexpected = frozenset(conf['unexpected'])
-        self.__waited = set(self.__expected)
-        assert len(self.__expected & self.__unexpected) == 0
+        self.__patterns = set(Pattern.of(e) for e in conf['expected'])
+        self.__expected = frozenset(self.__patterns)
 
     def __str__(self) -> str:
         return f'expected: {self.__expected}, unexpected: {self.__unexpected}'
@@ -47,22 +99,28 @@ class Rules:
         for s in self.__unexpected:
             if s in line:
                 if self.__verbose:
-                    fatal(f'detecting unexpected string "{s}"', file=sys.stdout)
-                raise RulesNotSatisified(f'unexpected strings detected')
-        for s in self.__expected:
-            if s in line:
-                self.__waited.remove(s)
+                    fatal(f'detecting unexpected pattern "{s}"', file=sys.stdout)
+                raise RulesNotSatisified(f'unexpected pattern detected')
+        rmset = set()
+        for pattern in self.__patterns:
+            matched, picked, picked_str = pattern(line)
+            if picked:
                 if self.__verbose:
-                    info(f'detecting expected string "{s}"')
-
-        return len(self.__waited)
+                    info(f'picking string "{picked_str}"')
+                if matched:
+                    if self.__verbose:
+                        info(f'detecting expected pattern "{str(pattern)}"')
+                    rmset.add(pattern)
+                break
+        self.__patterns -= rmset
+        return len(self.__patterns)
 
     def __enter__(self, *_):
         return self
 
     def __exit__(self, *_):
-        if len(self.__waited) != 0:
-            raise RulesNotSatisified(f'missing expected strings {self.__waited}')
+        if len(self.__patterns) != 0:
+            raise RulesNotSatisified(f'missing expected pattern {str(self.__patterns)}')
 
 
 def eliminate_child(ch: subprocess.Popen[bytes], verbose: bool):
