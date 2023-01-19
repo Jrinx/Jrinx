8aca59c101acd72e21319541b212ed7a5d28b907
coekjan <cn_yzr@qq.com>
2023-01-20 00:14:00 +0800

    实现并使用可嵌套的评测配置规则

--- scripts/python/judge.py
+++ scripts/python/judge.py
@@ -6,9 +6,9 @@ import argparse
 import json
 import os
 import psutil
+import re
 import signal
 import subprocess
-from typing import Sequence
 
 from utils import *
 
@@ -46,11 +46,12 @@ class Pattern:
         self._pattern = pattern
 
     @abstractmethod
-    def __call__(self, s: str) -> tuple[bool, bool, str]:  # Matched, Picked, PickedStr
+    def __call__(self, s: str) -> tuple[bool, bool]:  # Retire, Pick
         pass
 
+    @abstractmethod
     def __str__(self) -> str:
-        return str(self._pattern)
+        pass
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -60,68 +61,96 @@ class Pattern:
         match pattern:
             case str():
                 return StringPattern(pattern)
-            case tuple() | list():
-                return SeqPattern(pattern)
+            case {'type': typ, **__}:
+                return eval(f'{typ.title()}Pattern')(pattern)
             case _:
                 raise PatternTypeError(f'Unsupported pattern type {type(pattern)}')
 
 
 class StringPattern(Pattern):
-    def __call__(self, s: str) -> tuple[bool, bool, str]:
-        if self._pattern in s:
-            return True, True, s
-        return False, False, None
-
-
-class SeqPattern(Pattern):
-    def __init__(self, pattern: Sequence[str]) -> None:
+    def __init__(self, pattern) -> None:
         super().__init__(pattern)
+        self.__regex: re.Pattern = re.compile(pattern)
+
+    def __str__(self) -> str:
+        return f'(re: {self.__regex})'
+
+    def __call__(self, s: str) -> tuple[bool, bool]:
+        mat = self.__regex.search(s)
+        if mat:
+            return True, True
+        return False, False
+
+
+class OrderedPattern(Pattern):
+    def __init__(self, pattern) -> None:
+        super().__init__(pattern)
+        self.__vals = tuple(Pattern.of(p) for p in pattern['vals'])
         self.__next = 0
 
-    def __call__(self, s: str) -> tuple[bool, bool, str]:
-        pattern = self._pattern[self.__next]
-        if pattern in s:
-            self.__next += 1
-            return self.__next >= len(self._pattern), True, pattern
-        return False, False, None
+    def __str__(self) -> str:
+        return f'(ordered: {self.__vals})'
+
+    def __call__(self, s: str) -> tuple[bool, bool]:
+        retire, pick = self.__vals[self.__next](s)
+        if pick:
+            if retire:
+                self.__next += 1
+            return self.__next >= len(self.__vals), True
+        return False, False
+
+
+class UnorderedPattern(Pattern):
+    def __init__(self, pattern) -> None:
+        super().__init__(pattern)
+        self.__wait = set(Pattern.of(p) for p in pattern['vals'])
+        self.__all = frozenset(self.__wait)
+
+    def __str__(self) -> str:
+        return f'(unordered: {self.__all})'
+
+    def __call__(self, s: str) -> tuple[bool, bool]:
+        rmset = set()
+        for p in self.__wait:
+            retire, pick = p(s)
+            if pick:
+                if retire:
+                    rmset.add(p)
+        self.__wait -= rmset
+        return len(self.__wait) == 0, len(rmset) != 0
 
 
 class Rules:
     def __init__(self, verbose: bool, conf) -> None:
         self.__verbose = verbose
-        self.__unexpected = frozenset(conf['unexpected'])
-        self.__patterns = set(Pattern.of(e) for e in conf['expected'])
-        self.__expected = frozenset(self.__patterns)
+        self.__expected_patterns = Pattern.of(conf['expected'])
+        self.__unexpected_patterns = Pattern.of(conf['unexpected'])
+        self.__done = False
 
-    def __str__(self) -> str:
-        return f'expected: {self.__expected}, unexpected: {self.__unexpected}'
-
-    def __call__(self, line: str) -> int:
-        for s in self.__unexpected:
-            if s in line:
+    def __call__(self, line: str) -> bool:
+        retire, pick = self.__unexpected_patterns(line)
+        if pick:
+            if retire:
                 if self.__verbose:
-                    fatal(f'detecting unexpected pattern "{s}"', file=sys.stdout)
+                    fatal(f'detecting unexpected pattern \
+"{self.__unexpected_patterns}"', file=sys.stdout)
                 raise RulesNotSatisified(f'unexpected pattern detected')
-        rmset = set()
-        for pattern in self.__patterns:
-            matched, picked, picked_str = pattern(line)
-            if picked:
+        retire, pick = self.__expected_patterns(line)
+        if pick:
+            if self.__verbose:
+                info(f'picking expected string')
+            if retire:
                 if self.__verbose:
-                    info(f'picking string "{picked_str}"')
-                if matched:
-                    if self.__verbose:
-                        info(f'detecting expected pattern "{str(pattern)}"')
-                    rmset.add(pattern)
-                break
-        self.__patterns -= rmset
-        return len(self.__patterns)
+                    info(f'detecting expected pattern "{self.__expected_patterns}"')
+        self.__done = retire
+        return retire
 
     def __enter__(self, *_):
         return self
 
     def __exit__(self, *_):
-        if len(self.__patterns) != 0:
-            raise RulesNotSatisified(f'missing expected pattern {str(self.__patterns)}')
+        if not self.__done:
+            raise RulesNotSatisified(f'missing expected pattern')
 
 
 def eliminate_child(ch: subprocess.Popen[bytes], verbose: bool):
@@ -199,6 +228,8 @@ def main():
 
     cmd = ['make', 'run']
     interactive = conf.get('interactive')
+    if interactive:
+        interactive = [{'when': Pattern.of(i['when']), 'send': i['send']} for i in interactive]
     ch = subprocess.Popen(cmd,
                           stdin=subprocess.PIPE if interactive else None,
                           stdout=subprocess.PIPE,
@@ -222,20 +253,19 @@ after all expected strings detected'
 
             signal.signal(signal.SIGALRM, on_timeout)
             signal.alarm(CHILD_TIMEOUT)
-
-            if verbose:
-                info(str(rules_check))
             for out in ch.stdout:
                 line = out if isinstance(out, str) else out.decode('utf-8')
                 if verbose and not suppress_emu_out:
                     sys.stdout.write(line)
-                if interactive and interactive[0]['when'] in line:
-                    ch.stdin.write(interactive[0]['send'].encode('utf-8'))
-                    ch.stdin.flush()
-                    interactive = interactive[1:]
+                if interactive:
+                    retire, _ = interactive[0]['when'](line)
+                    if retire:
+                        ch.stdin.write(interactive[0]['send'].encode('utf-8'))
+                        ch.stdin.flush()
+                        interactive = interactive[1:]
                 if final_stuck and len(line) != 0:
                     rules_check(line)
-                if not final_stuck and rules_check(line) == 0:
+                if not final_stuck and rules_check(line):
                     final_stuck = True
                     signal.alarm(CHILD_FINAL_STUCK_TIMEOUT)
     except JudgeTimeout as e:
