98e37545a3fee37a093d74cacbb0014457d3211f
coekjan <cn_yzr@qq.com>
2023-03-19 01:11:42 +0800

    将 `asid-test` 中的静态大数组改为动态数组，减小内核文件大小

--- kern/tests/asid-test.c
+++ kern/tests/asid-test.c
@@ -1,20 +1,21 @@
+#include <bitmap.h>
 #include <kern/lib/debug.h>
 #include <kern/lib/errors.h>
 #include <kern/mm/asid.h>
 #include <kern/tests.h>
 #include <lib/string.h>
 
-static unsigned asid_allocated[0x10000U];
-static unsigned asid_allocated_max = 0;
-
 static void asid_test(void) {
   info("test allocation\n");
+  BITMAP_DECL(asid_allocated, asid_get_max() + 1);
+  memset(asid_allocated, 0, sizeof(asid_allocated));
+  unsigned asid_allocated_max = 0;
   unsigned long asid;
   for (unsigned i = 0; i <= asid_get_max(); i++) {
     assert(asid_alloc(&asid) == KER_SUCCESS);
     assert(asid >= 0 && asid <= asid_get_max());
-    assert(asid_allocated[asid] == 0);
-    asid_allocated[asid] = 1;
+    assert(bitmap_get_bit(asid_allocated, asid) == 0);
+    bitmap_set_bit(asid_allocated, asid);
     asid_allocated_max = asid > asid_allocated_max ? asid : asid_allocated_max;
   }
   assert(asid_allocated_max == asid_get_max());
