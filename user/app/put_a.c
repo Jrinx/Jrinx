bdff1f7f54d96554033a4c6244f1d8bb441b9b17
coekjan <cn_yzr@qq.com>
2023-04-18 17:39:45 +0800

    添加多分区的 put_{a|b} 程序与相应的测试

---
+++ user/app/put_a.c
@@ -0,0 +1,32 @@
+#include <layouts.h>
+#include <user/lib/debug.h>
+#include <user/lib/logger.h>
+#include <user/sys/services.h>
+#include <user/sys/syscalls.h>
+
+void put_a(void) {
+  RETURN_CODE_TYPE ret;
+  info("put 'A' to console\n");
+  for (size_t i = 0; i < 20; i++) {
+    sys_cons_write_char('A');
+    SUSPEND_SELF(150 * SYS_TIME_MILLISECOND, &ret);
+  }
+  halt("done\n");
+}
+
+void main(void) {
+  PROCESS_ATTRIBUTE_TYPE attr = {
+      .NAME = "put-a",
+      .ENTRY_POINT = (SYSTEM_ADDRESS_TYPE)put_a,
+      .BASE_PRIORITY = 2,
+      .STACK_SIZE = PGSIZE * 4,
+      .DEADLINE = SOFT,
+      .PERIOD = SYSTEM_TIME_INFINITE_VAL,
+      .TIME_CAPACITY = SYSTEM_TIME_INFINITE_VAL,
+  };
+  PROCESS_ID_TYPE proc_id;
+  check_e(CREATE_PROCESS(&attr, &proc_id, &ret));
+  check_e(START(proc_id, &ret));
+  check_e(SET_PARTITION_MODE(NORMAL, &ret));
+  fatal("unreachable code\n");
+}
---
+++ user/app/put_b.c
@@ -0,0 +1,32 @@
+#include <layouts.h>
+#include <user/lib/debug.h>
+#include <user/lib/logger.h>
+#include <user/sys/services.h>
+#include <user/sys/syscalls.h>
+
+void put_b(void) {
+  RETURN_CODE_TYPE ret;
+  info("put 'B' to console\n");
+  for (size_t i = 0; i < 20; i++) {
+    sys_cons_write_char('B');
+    SUSPEND_SELF(150 * SYS_TIME_MILLISECOND, &ret);
+  }
+  halt("done\n");
+}
+
+void main(void) {
+  PROCESS_ATTRIBUTE_TYPE attr = {
+      .NAME = "put-b",
+      .ENTRY_POINT = (SYSTEM_ADDRESS_TYPE)put_b,
+      .BASE_PRIORITY = 2,
+      .STACK_SIZE = PGSIZE * 4,
+      .DEADLINE = SOFT,
+      .PERIOD = SYSTEM_TIME_INFINITE_VAL,
+      .TIME_CAPACITY = SYSTEM_TIME_INFINITE_VAL,
+  };
+  PROCESS_ID_TYPE proc_id;
+  check_e(CREATE_PROCESS(&attr, &proc_id, &ret));
+  check_e(START(proc_id, &ret));
+  check_e(SET_PARTITION_MODE(NORMAL, &ret));
+  fatal("unreachable code\n");
+}
